0f62d0caeac7449acc00b50ee269b3885aa646da
Romain Linsolas <linsolas@gmail.com>
2015-04-14 23:55:03 +0200

    Create BankTest.java

---
+++ src/test/java/com/github/linsolas/BankTest.java
@@ -0,0 +1,17 @@
+package com.github.linsolas;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class BankTest {
+
+    @Test
+    public void should_be_the_answer() {
+        int theAnswer = 42;
+        assertThat(theAnswer).isEqualTo(42);
+    }
+
+
+}
