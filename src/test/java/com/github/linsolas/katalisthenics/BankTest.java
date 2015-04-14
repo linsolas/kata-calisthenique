cdb31b6a86ba2f7813e8c7126da79b3bd5a36871
Romain Linsolas <linsolas@gmail.com>
2015-04-15 00:06:56 +0200

    Create BankTest.java

---
+++ src/test/java/com/github/linsolas/katalisthenics/BankTest.java
@@ -0,0 +1,15 @@
+package com.github.linsolas.katalisthenics;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BankTest {
+
+    @Test
+    public void should_be_the_answer() {
+        int theAnswer = 42;
+        assertThat(theAnswer).isEqualTo(42);
+    }
+
+}
